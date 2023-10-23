bb731ec10f7e9059188cf5cd665cffde04795652
nutzys <ulvisdulbergs@Ulviss-MacBook-Air.local>
2023-10-23 16:58:35 +0300

    credentials

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'Rms160ww!',
-    database: 'node-learning',
+    user: '',
+    password: '',
+    database: '',
 });
 
 module.exports = db;
